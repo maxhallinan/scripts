d7cb1cc712712faf9901044953eb7839d0ce0e6f
Max Hallinan <maxhallinan@gmail.com>
2024-10-11 17:00:14 -0400

    Add validation to fix-homebrew-postmasterpid

--- scripts/fix-homebrew-postmasterpid.sh
+++ scripts/fix-homebrew-postmasterpid.sh
@@ -1,9 +1,25 @@
 if [[ "$#" -ne 1 ]]; then
   command_name=$(basename "$0")
-  echo "Usage: $command_name <Homebrew formulae>
-For example, $command_name postgresql@14" >&2
+  echo "Usage: $command_name <Homebrew formulae>" >&2
+  echo "Example: $command_name postgresql@14" >&2
   exit 1
 fi
-brew services stop "$1"
-rm "$HOMEBREW_PREFIX/var/$1/postmaster.pid"
-brew services restart "$1"
+
+postgres_formulae="$1"
+data_directory="$HOMEBREW_PREFIX/var/$postgres_formulae"
+postmaster_pid="$data_directory/postmaster.pid"
+
+pg_ctl status --pgdata="$data_directory"
+
+if [[ $? -ne 3 ]]; then
+  echo "Error: Postgres is not stopped." >&2
+  exit 1
+fi
+
+if [[ ! -f $postmaster_pid ]]; then
+  echo "Error: $postmaster_pid file does not exist." >&2
+  exit 1
+fi
+
+rm "$postmaster_pid"
+brew services restart "$postgres_formulae"
